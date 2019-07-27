80936af9198e2dde5c697834cc38e7d982cd161e
kitagawa <mc2vers@gmail.com>
2019-07-28 04:35:42 +0900

    add sample

--- something_like_int/something_like_int.cpp
+++ something_like_int/something_like_int.cpp
@@ -5,12 +5,24 @@
 
 #include "cmpt/arithmetic_interface.hpp"
 
-int main()
-{
+int main(){
+	using namespace cmpt::arithmetic;
+	
+	if (1) {
+		// int のような型でインクリメント、デクリメントのみが可能な方を定義
+		class SomethingIncrementable : public Incrementable<StackBase<SomethingIncrementable, int>> {};
+
+		auto i2 = SomethingIncrementable().set(2);
+		++i2;	// インクリメント可能
+		i2--;	// デクリメント可能
+		SomethingIncrementable i2_copy = i2;	// コピー可能
+
+		// i2 = i2 + i2;					// エラー 加算は定義されていない 
+		// SomethingIncrementable i3{3};	// エラー もととなる方からのコピー不可
+		// int i = i2;						// エラー もととなる方へのコピー不可
+	}
 
 	if (1) {
-		using namespace cmpt::arithmetic;
-
 		// int のような型でインクリメント、デクリメント、加算、乗算、比較 が可能なものを定義
 		class SomethingLikeInt : public Comparable<Productable<Addable<Incrementable<StackBase<SomethingLikeInt, int>>>>> {};
 
@@ -24,10 +36,7 @@ int main()
 		std::cout << (i2 < i3) << std::endl;
 		std::cout << (i2 == i3) << std::endl;
 	}
-
-	if (1) {
-
-	}
+	
 	return 0;
 }
 
